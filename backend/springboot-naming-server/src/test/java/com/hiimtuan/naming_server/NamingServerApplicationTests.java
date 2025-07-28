91395984d2087b07f760639552c50dfc08e17140
NguyenTuan <nguyentuanwd.ou@gmail.com>
2025-07-28 09:12:10 +0700

    First commit

---
+++ backend/springboot-naming-server/src/test/java/com/hiimtuan/naming_server/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiimtuan.naming_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
