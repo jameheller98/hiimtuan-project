47621bcf5fc1f45c46dbd2534dafd745b2e6a85d
NguyenTuan <nguyentuanwd.ou@gmail.com>
2025-08-22 01:52:25 +0700

    add: handle feature refresh token

--- backend/springboot-post-service/src/main/java/com/hiimtuan/post_service/PostServiceApplication.java
+++ backend/springboot-post-service/src/main/java/com/hiimtuan/post_service/PostServiceApplication.java
@@ -2,8 +2,10 @@ package com.hiimtuan.post_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.grpc.client.ImportGrpcClients;
 
 @SpringBootApplication
+@ImportGrpcClients(basePackageClasses = PostServiceApplication.class)
 public class PostServiceApplication {
 
 	public static void main(String[] args) {
