95269cabe503507e4b29861d98e2e81da8817612
jameheller98 <nguyentuanwd.ou@gmail.com>
2026-01-18 11:53:03 +0700

    upgrade version nextjs and fix bug path register api

--- frontend/hiimtuan-portfolio-web/src/api/mutations/auth/useRegisterMutation.ts
+++ frontend/hiimtuan-portfolio-web/src/api/mutations/auth/useRegisterMutation.ts
@@ -4,16 +4,16 @@ import { IApiResponse } from "@/api/api.interface";
 
 async function register(
   url: string,
-  { arg }: { arg: { fullName: string; email: string; password: string } }
+  { arg }: { arg: { fullName: string; email: string; password: string } },
 ) {
   return api.post<IApiResponse<string>, { email: string; password: string }>(
     url,
-    arg
+    arg,
   );
 }
 
 export function useRegisterMutation() {
-  const { trigger } = useSWRMutation(`/auth/register`, register);
+  const { trigger } = useSWRMutation(`/user/register`, register);
 
   return {
     trigger,
